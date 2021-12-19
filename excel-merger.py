c2aed5c44310f0216f71bdf58425590d58b2e077
AlexLuevano <alejanlu2502@gmail.com>
2021-12-18 23:13:57 -0700

    Formatting

--- excel-merger.py
+++ excel-merger.py
@@ -1,20 +1,21 @@
-import pandas as pd
-import xlsxwriter
-from tkinter import *
-from tkinter import filedialog
-from sys import exit
 import os
 from datetime import datetime
-from time import strftime, gmtime
+from sys import exit
+from time import gmtime, strftime
+from tkinter import *
+from tkinter import filedialog
+
+import pandas as pd
+import xlsxwriter
 
 window = Tk()
-files = filedialog.askopenfilenames(parent = window, title = 'Escoge archivos')
+files = filedialog.askopenfilenames(parent=window, title='Escoge archivos')
 files_str = str(files)
 files_list = list(files)
 window.destroy()
 window.mainloop()
 
-#print(files_list)
+# print(files_list)
 
 if os.path.exists('C:\\temporary'):
     pass
@@ -35,27 +36,31 @@ else:
 now = datetime.now()
 date = now.strftime("%Y-%m-%d_%H-%M-%S")
 #path = os.path.join('C:\\Users',os.getlogin(),'Desktop','xls_merged_' + str(date)+'.xlsx')
-path = os.path.join('C:\\temporary','xls_merged_' + str(date)+'.xlsx')
+path = os.path.join('C:\\temporary', 'xls_merged_' + str(date)+'.xlsx')
 df = pd.DataFrame()
-writer = pd.ExcelWriter(path,engine='xlsxwriter')
+writer = pd.ExcelWriter(path, engine='xlsxwriter')
 
 for f in files_list:
     if f[-4:] == '.csv':
-        data = pd.read_csv(f, index_col=False, header=0, na_filter = False)
+        data = pd.read_csv(f, index_col=False, header=0, na_filter=False)
         df = df.append(data)
         print(f'{f} merged')
     else:
         try:
-            data = pd.read_excel(f,sheet_name='Entities', index_col=False, header=1, na_filter = False)
+            data = pd.read_excel(f, sheet_name='Entities',
+                                 index_col=False, header=1, na_filter=False)
             df = df.append(data)
+            print(f'{f} merged')
         except:
-            print('Sheet "Entitites" not found. Merging the first sheet of the file instead.')
-            data = pd.read_excel(f,index_col=False, na_filter = False)
+            print(
+                'Sheet "Entitites" not found. Merging the first sheet of the file instead.')
+            data = pd.read_excel(f, index_col=False, na_filter=False)
             df = df.append(data)
+            print(f'{f} merged')
 
 print('All files read and merged')
-df.to_excel(writer,sheet_name='Entities',index = False, header = True)
+df.to_excel(writer, sheet_name='Entities', index=False, header=True)
 writer.save()
 print('File saved. Please review file at C:\\temporary')
-#writer.close()
+# writer.close()
 exit(0)
